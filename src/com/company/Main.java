d2469ed51b623fbea1e42e016bbacd678071c0df
agustinintro11 <agustinintro_123@hotmail.com>
2018-09-21 20:37:57 -0300

    Default Changelist

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,65 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
+    private Scanner scanner = new Scanner(System.in);
+
+    public void menu(){
+        Sistema sistema = new Sistema();
+        boolean salir = false;
+
+        while(!salir){
+            System.out.println("1 - Registrar jugador");
+            System.out.println("2 - Jugar partida");
+            System.out.println("3 - Replicar partida");
+            System.out.println("4 - Ranking");
+            System.out.println("0 - Salir");
+            System.out.print("Eleccion: ");
+
+            String op = scanner.nextLine();
+
+            switch(op){
+                case "1":
+                    //TODO: Validar alias unico, y todo lo demas jaja
+                    System.out.println("Ingrese nombre:");
+                    String nombre = scanner.nextLine();
+                    System.out.println("Ingrese alias:");
+                    String alias = scanner.nextLine();
+                    System.out.println("Ingrese edad:");
+                    int edad = scanner.nextInt();
+
+                    scanner.nextLine();
+                    Jugador nuevoJugador=new Jugador(nombre,alias,edad);
+
+                    sistema.registrarJugador(nuevoJugador);
+                    break;
+                case "2":
+                    //TODO: validar que existe con ese alias sino seguir preguntando juan ...
+                    System.out.println("Ingrese alias del primer jugador: ");
+                    String aliasJugador = scanner.nextLine();
+                    Jugador j1 = sistema.buscarJugador(aliasJugador);
+                    System.out.println("Ingrese alias del segundo jugador: ");
+                    aliasJugador = scanner.nextLine();
+                    Jugador j2 = sistema.buscarJugador(aliasJugador);
+
+                    sistema.jugar(j1, j2);
+                    break;
+                case "3":
+                    sistema.replicar();
+                    break;
+                case "4":
+                    sistema.ranking();
+                    break;
+                default:
+                    salir = true;
+            }
+        }
+
+    }
 
     public static void main(String[] args) {
         Sistema sistema = new Sistema();
-        sistema.empezar();
+
     }
 }
--- src/com/company/Sistema.java
+++ src/com/company/Sistema.java
@@ -7,76 +7,26 @@ public class Sistema {
     private ArrayList<Jugador> jugadores = new ArrayList<Jugador>();
     private Scanner scanner = new Scanner(System.in);
 
-    public void empezar(){
-        boolean salir = false;
 
-        while(!salir){
-            System.out.println("1 - Registrar jugador");
-            System.out.println("2 - Jugar partida");
-            System.out.println("3 - Replicar partida");
-            System.out.println("4 - Ranking");
-            System.out.println("0 - Salir");
-            System.out.print("Eleccion: ");
 
-            String op = scanner.nextLine();
-
-            switch(op){
-                case "1":
-                    //TODO: Validar alias unico, y todo lo demas jaja
-                    System.out.println("Ingrese nombre:");
-                    String nombre = scanner.nextLine();
-                    System.out.println("Ingrese alias:");
-                    String alias = scanner.nextLine();
-                    System.out.println("Ingrese edad:");
-                    int edad = scanner.nextInt();
-
-                    scanner.nextLine();
-
-                    registrarJugador(nombre,alias,edad);
-                    break;
-                case "2":
-                    //TODO: validar que existe con ese alias sino seguir preguntando juan ...
-                    System.out.println("Ingrese alias del primer jugador: ");
-                    String aliasJugador = scanner.nextLine();
-                    Jugador j1 = buscarJugador(aliasJugador);
-                    System.out.println("Ingrese alias del segundo jugador: ");
-                    aliasJugador = scanner.nextLine();
-                    Jugador j2 = buscarJugador(aliasJugador);
-
-                    jugar(j1, j2);
-                    break;
-                case "3":
-                    replicar();
-                    break;
-                case "4":
-                    ranking();
-                    break;
-                default:
-                    salir = true;
-            }
-        }
-
+    public void registrarJugador(Jugador jugador){
+        jugadores.add(jugador);
     }
 
-    private void registrarJugador(String nombre, String alias, int edad){
-        Jugador nuevoJugador = new Jugador(nombre, alias, edad);
-        jugadores.add(nuevoJugador);
-    }
-
-    private void jugar(Jugador jugador1, Jugador jugador2){
+    public void jugar(Jugador jugador1, Jugador jugador2){
         Partida partida = new Partida(jugador1, jugador2);
         partida.comenzar();
     }
 
-    private void replicar(){
+    public void replicar(){
         System.out.println("Replicar");
     }
 
-    private void ranking(){
+    public void ranking(){
         System.out.println("Ranking");
     }
 
-    private Jugador buscarJugador(String alias){
+    public Jugador buscarJugador(String alias){
         for(Jugador jugador : jugadores)
             if(jugador.getAlias().equals(alias))
                 return jugador;
